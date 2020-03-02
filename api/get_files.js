fecdf7020d7ee0849c38556f8b6c567c0f92a26a
Alejandro Sanchez <aalejo@gmail.com>
2020-03-01 23:08:46 -0500

    connection with uploadcare withing the same api

--- api/get_files.js
+++ api/get_files.js
@@ -1,12 +1,27 @@
-var uploadcare = require('../lib/main')(process.env.UPLOAD_CARE_PUBLIC, private.env.UPLOAD_CARE_KEY),
+var uploadcare = require('uploadcare')(process.env.UPLOAD_CARE_PUBLIC, process.env.UPLOAD_CARE_KEY),
 fs = require('fs');
 module.exports = (req, res) => {
-    console.log("test");
     const page = req.query.page ? req.query.page : 1;
-    const limit = req.query.limit ? req.query.limit : 1;
-    uploadcare.files.list({page, limit}, (resp, error) => {
-        console.log('resp', resp);
+    const limit = req.query.limit ? req.query.limit : 100;
+    res.setHeader('content-type', 'application/json');
+    uploadcare.files.list({page, limit}, (error, data) => {
+        if(error){
+            
+            res.send(JSON.stringify(data));
+        }
+        else{
+            const images = data.results.map(i => {
+                return {
+                    url: i.url,
+                    uuid: i.uuid,
+                    description: "no description",
+                    category: "uknown",
+                    created_at: i.datetime_uploaded
+                }
+            });
+            res.send(JSON.stringify(images));
+        }
         console.log('error', error);
-        res.send(`Hello!`)
+        console.log('data', data);
     });
 }
