02014b4b18a89a2e83b4fc286dc1b89f14b3ad37
Alejandro Sanchez <aalejo@gmail.com>
2020-03-01 22:23:27 -0500

    removed dot env

--- api/get_files.js
+++ api/get_files.js
@@ -1,6 +1,7 @@
 var uploadcare = require('../lib/main')(process.env.UPLOAD_CARE_PUBLIC, private.env.UPLOAD_CARE_KEY),
 fs = require('fs');
 module.exports = (req, res) => {
+    console.log("test");
     const page = req.query.page ? req.query.page : 1;
     const limit = req.query.limit ? req.query.limit : 1;
     uploadcare.files.list({page, limit}, (resp, error) => {
